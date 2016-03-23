373d99379a6403ac9a6118c139283c904a0f9142
Jittaranan Tipboonde <jitinics@NICs-Jittaranans-MacBook-Air.local>
2016-03-23 17:15:42 +0700

    fix port listen for heroku

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express')
 var app = express()
 var port = 8080
 app.use(express.static(__dirname))
-app.listen(port, function () {
-  console.log('Example app listening on port %d!', port)
+app.listen(process.env.PORT || port, function () {
+  console.log(' app listening on port %d!', port)
 })
